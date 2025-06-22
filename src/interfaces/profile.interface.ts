677efea89aad42b2e1e8d2651d980f3592bd1222
Beryl Atieno <107188748+BerylCAtieno@users.noreply.github.com>
2025-06-22 17:37:05 +0300

    Revert "User Features"

--- src/interfaces/profile.interface.ts
+++ src/interfaces/profile.interface.ts
@@ -1,16 +1,8 @@
-import { Preference} from '../generated/prisma'
-
-export interface UpdateProfileInterface{
-    firstName?: string
-    otherName?: string
-    lastName?: string
-    phoneNumber?: string
-    street?: string
-    city?:string
-    state?:string
-    NIN?:string
-    BVN?:string
-    OtherInfo?:string
-    address?:string
-    preferences?: Preference
+export interface tenantProfileInterface {
+    fullName: string,
+    phoneNumber: string,
+    street: string,
+    city:string,
+    state:string,
+    NIN:string,
 }
